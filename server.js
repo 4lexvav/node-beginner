df310fd82609f84391d85c1066964beaacdfa38f
Glitch (calc-bot) <none>
2018-04-09 14:57:15 +0000

    👞🐿 Updated with Glitch

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var http = require('http');
+var url  = require('url');
 
-function start() {
+function start(route, handle) {
   function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {'Content-type': 'text/plain'});
-    response.write('Hello World!');
-    response.end();
+    var postData = '';
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    
+    request.setEncoding('utf8');
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '"+  postDataChunk + "'.");
+    });
+    
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData);
+    });
   }
-  console.log("Server has started.");
   
-  http.createServer(onRequest).listen(process.env.Port);
+  console.log("Server has started.");
+  http.createServer(onRequest).listen(process.env.PORT);
 }
 
 exports.start = start;
